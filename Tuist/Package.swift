84de038cd84d1d2d3b3f9276cb90971072b3abf2
talq44 <talq44@gmail.com>
2025-03-17 09:58:17 +0900

    firebase 추가

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -18,5 +18,9 @@ let package = Package(
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        .package(
+            url: "https://github.com/firebase/firebase-ios-sdk",
+            .upToNextMinor(from: "11.8.0")
+        )
     ]
 )
